f660d959a5ea58a28077887345b76fdc042c265c
jaskaransing108373 <jaskaransing108373@github.com>
2021-02-03 09:09:37 -0500

    added base

---
+++ css/main.js
@@ -0,0 +1,47 @@
+import ChatMessage from "./components/TheMessageComponent.js"
+
+(() => {
+    console.log('fired');
+
+    // load socket library and make a connection 
+    const socket = io();
+
+    // messenger service event handling -> incoming from the manager
+    function setUserId({sID, message}) {
+        // incoming connected event with data
+       // debugger; 
+        vm.socketID = sID;
+    }
+
+    function appendMessage(message) {
+        vm.messages.push(message);
+    }
+    const vm = new Vue({
+        data: {
+            messages: [],
+            nickname: "",
+            username: "",
+            socketID: "",
+            message: ""
+        },
+
+        created: function() {
+            console.log('its alive!!');
+        },
+
+        methods: {
+            dispatchMessage() {
+                debugger;
+                socket.emit('chatmessage', { content: this.message, name: this.nickname || 'Anonymous'})
+            }
+        },
+
+        components: {
+            newmessage: ChatMessage
+        }
+
+    }).$mount("#app");
+
+    socket.addEventListener("connected", setUserId);
+    socket.addEventListener('message', appendMessage);
+})();
